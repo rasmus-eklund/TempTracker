8d0ca43579a6aa1e1295777f574a04d1c4be090b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-12 14:07:58 +0100

    add data schema

--- src/zodSchemas.ts
+++ src/zodSchemas.ts
@@ -1,9 +1,15 @@
 import { z } from "zod";
 
-export const tempSchema = z.object({
+export const tempSchemaId = z.object({
   id: z.string(),
-  temp: z.number().positive(),
-  date: z.date(),
+  temp: z.coerce.number().positive(),
+  date: z.coerce.date(),
+});
+export type TempId = z.infer<typeof tempSchemaId>;
+
+export const tempSchema = z.object({
+  temp: z.coerce.number().positive(),
+  date: z.coerce.date(),
 });
 export type Temp = z.infer<typeof tempSchema>;
 
