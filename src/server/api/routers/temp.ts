d0362589bd78e0c07f915cb672cd121d0cb5e714
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-12 14:15:11 +0100

    data crud

--- src/server/api/routers/temp.ts
+++ src/server/api/routers/temp.ts
@@ -1,12 +1,14 @@
+import { createId } from "@paralleldrive/cuid2";
 import { eq } from "drizzle-orm";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { temps } from "~/server/db/schema";
-import { idSchema, tempSchema } from "~/zodSchemas";
+import { idSchema, tempSchema, tempSchemaId } from "~/zodSchemas";
 
 export const postRouter = createTRPCRouter({
   create: protectedProcedure
     .input(tempSchema)
-    .mutation(async ({ ctx, input: { temp, date, id } }) => {
+    .mutation(async ({ ctx, input: { temp, date } }) => {
+      const id = createId();
       await ctx.db.insert(temps).values({
         id,
         temp,
@@ -16,11 +18,16 @@ export const postRouter = createTRPCRouter({
     }),
 
   read: protectedProcedure.query(async ({ ctx }) => {
-    return await ctx.db.query.temps.findMany();
+    const userId = ctx.session.user.id;
+    const data = await ctx.db
+      .select()
+      .from(temps)
+      .where(eq(temps.createdById, userId));
+    return data.map(({ temp, createdAt }) => ({ date: createdAt, temp }));
   }),
 
   update: protectedProcedure
-    .input(tempSchema)
+    .input(tempSchemaId)
     .mutation(async ({ ctx, input: { date, id, temp } }) => {
       await ctx.db.update(temps).set({ id, temp, updatedAt: date });
     }),
