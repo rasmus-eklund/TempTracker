dded3cc9095cedd47baab15e948c7f612e68bdbf
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-12 14:13:41 +0100

    style

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -4,12 +4,12 @@ import { signIn } from "next-auth/react";
 const Login = () => {
   return (
     <button
-      className="cursor-pointer rounded-md bg-black/80 p-2 px-6 text-3xl text-white md:hover:bg-gray-400"
+      className="cursor-pointer rounded-md bg-black/80 p-2 px-6 text-white md:hover:bg-gray-400"
       onClick={async () => {
         await signIn();
       }}
     >
-      Logga in
+      Login
     </button>
   );
 };
