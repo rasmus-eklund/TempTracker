cde1bf74dac6f9b76c66fbd32ac5eb4f524855cd
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-14 12:29:51 +0100

    Add color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,14 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      c1: "#FFE4D6",
+      c2: "#FACBEA",
+      c3: "#D988B9",
+      c4: "#B0578D",
+      black: "#000000",
+      white: "#FFFFFF"
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
