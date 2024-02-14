bda3874a1d0c13e34b124705d19c6a4a7d870f68
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-14 12:34:17 +0100

    use button

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { signIn } from "next-auth/react";
+import Button from "./Button";
 
 const Login = () => {
   return (
-    <button
+    <Button
       className="cursor-pointer rounded-md bg-black/80 p-2 px-6 text-white md:hover:bg-gray-400"
       onClick={async () => {
         await signIn();
       }}
     >
       Login
-    </button>
+    </Button>
   );
 };
 
