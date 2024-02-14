a6cf8c9fe065e5a0bab77bc98fd2461da61571ad
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-14 12:32:34 +0100

    Refactor: remove routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 import { unstable_noStore as noStore } from "next/cache";
-import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
-
+import { api } from "~/trpc/server";
+import PlotTemp from "./_components/PlotTemp";
+import Sample from "./_components/Sample";
+import AddTempButton from "./_components/AddTempButton";
 const Home = async () => {
   noStore();
   const session = await getServerAuthSession();
+  const data = await api.temp.read.query();
 
   if (session) {
     return (
-      <main className="flex">
-        <Link href={"/temps/add"}>Add</Link>
-        <Link href={"/temps/view"}>View</Link>
+      <main className="bg-c1 flex max-w-4xl flex-col gap-4 p-5">
+        <PlotTemp data={data.sort((a, b) => Number(a.date) - Number(b.date))} />
+        <AddTempButton />
+        <ul className="flex flex-col gap-2">
+          {data
+            .sort((a, b) => Number(b.date) - Number(a.date))
+            .map((item) => (
+              <Sample key={crypto.randomUUID()} item={item} />
+            ))}
+        </ul>
       </main>
     );
   }
