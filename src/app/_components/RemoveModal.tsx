29e911f4995a86884be15854c8e49ed7dbba843c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-14 12:34:47 +0100

    Remove sample modal

---
+++ src/app/_components/RemoveModal.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ClipLoader } from "react-spinners";
+import Button from "./Button";
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
+
+type Props = { toggleModal: () => void; id: string };
+
+const RemoveModal = ({ id, toggleModal }: Props) => {
+  const router = useRouter();
+  const { mutate: remove, isLoading: removing } = api.temp.delete.useMutation({
+    onSuccess: () => router.refresh(),
+  });
+  return (
+    <div className="bg-3 flex flex-col items-center gap-5 p-5">
+      <p>Remove measurement?</p>
+      <div className="flex gap-5">
+        {removing ? (
+          <ClipLoader size={18} />
+        ) : (
+          <Button disabled={removing} onClick={() => remove({ id })}>
+            Delete
+          </Button>
+        )}
+        <Button onClick={toggleModal}>Cancel</Button>
+      </div>
+    </div>
+  );
+};
+
+export default RemoveModal;
