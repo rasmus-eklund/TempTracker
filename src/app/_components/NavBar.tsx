83fd5ea5143c325ba1a741d147a701f46e8811bf
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-16 08:36:42 +0100

    use component

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,31 +1,13 @@
-import Image from "next/image";
 import { getServerAuthSession } from "~/server/auth";
-import Login from "./Login";
+import { Login } from "./Auth";
+import UserCard from "./UserCard";
 
 const Header = async () => {
   const session = await getServerAuthSession();
-  if (!session) {
-    return (
-      <header className="flex items-center justify-between bg-black/80 p-4">
-        <h1>WamPerature</h1>
-        <Login />
-      </header>
-    );
-  }
   return (
-    <header className="bg-c3 flex items-center justify-between p-4">
-      {session.user.image ? (
-        <Image
-          className="h-8 w-8 rounded-full"
-          src={session.user.image}
-          height={250}
-          width={250}
-          alt="Profile Picture"
-        />
-      ) : (
-        <div className="bg-c4 h-8 w-8 rounded-full"></div>
-      )}
-      <h1>{session.user.name}</h1>
+    <header className="flex items-center justify-between bg-c3 p-4">
+      <h1>WamPerature</h1>
+      {session ? <UserCard user={session.user} /> : <Login />}
     </header>
   );
 };
