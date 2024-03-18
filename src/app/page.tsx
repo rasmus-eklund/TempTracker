f8131b38aab2fd7fd2547b816469a715d91ae1b8
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-18 11:58:05 +0100

    Feat: add dates filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,42 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
-import PlotTemp from "./_components/PlotTemp";
+import TempSvg from "./_components/TempSvg";
 import Sample from "./_components/Sample";
 import AddTempButton from "./_components/AddTempButton";
+import FilterDates from "./_components/FilterDates";
+import parseSearch from "./utils/parseUrlDates";
 
-const Home = async () => {
-  noStore();
-  const session = await getServerAuthSession();
-  if (session) {
-    return <View />;
-  }
-  return (
-    <main className="flex items-center justify-center">
-      <p>Login to access your data.</p>
-    </main>
-  );
+type Props = {
+  searchParams: Record<string, string | string[] | undefined>;
 };
 
-const View = async () => {
-  const data = await api.temp.read.query();
-  return (
-    <main className="flex grow flex-col gap-4 bg-c1 p-5">
-      <PlotTemp data={data} />
-      <AddTempButton />
-      <ul className="flex flex-col gap-2">
-        {data.reverse().map((item) => (
-          <Sample key={crypto.randomUUID()} item={item} />
-        ))}
-      </ul>
-    </main>
-  );
+const Home = async ({ searchParams }: Props) => {
+  noStore();
+  const session = await getServerAuthSession();
+  if (!session) {
+    return (
+      <main className="flex items-center justify-center">
+        <p>Login to access your data.</p>
+      </main>
+    );
+  }
+  const dates = parseSearch({ searchParams });
+  const data = await api.temp.read.query(dates);
+  if (session) {
+    return (
+      <main className="flex grow flex-col gap-4 bg-c1 p-5">
+        <FilterDates />
+        <TempSvg data={data} />
+        <AddTempButton />
+        <ul className="flex flex-col gap-2">
+          {data.reverse().map((item) => (
+            <Sample key={item.id} item={item} />
+          ))}
+        </ul>
+      </main>
+    );
+  }
 };
 
 export default Home;
