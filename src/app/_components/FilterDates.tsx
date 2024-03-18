cc02b06006e0736f669b30b84329e1f602772b17
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-18 11:58:57 +0100

    Feat: dates filter

--- src/app/_components/FilterDates.tsx
+++ src/app/_components/FilterDates.tsx
@@ -1,46 +1,60 @@
 "use client";
-import { type ReactNode, useState } from "react";
-import { formatDate } from "./generateDefaultTempAndDate";
+import { useRouter } from "next/navigation";
+import { dateToString } from "../utils/formatData";
+import { useState } from "react";
+import Button from "./Button";
+import { type FromTo } from "~/zodSchemas";
 
-type Filter = {
-  start: Date;
-  end: Date;
-};
-type Props = {
-  children: ({ filter }: { filter: Filter }) => ReactNode;
-};
+const FilterDates = () => {
+  const router = useRouter();
 
-const FilterDates = ({ children }: Props) => {
-  const [filter, setFilter] = useState<Filter>({
-    start: new Date("1970-01-01"),
-    end: new Date(),
+  const changeDate = ({ from, to }: FromTo) => {
+    router.push(`/?from=${dateToString(from)}&to=${dateToString(to)}`);
+  };
+
+  const firstDayOfThisMonth = new Date();
+  const lastDayOfThisMonth = new Date(
+    firstDayOfThisMonth.getFullYear(),
+    firstDayOfThisMonth.getMonth() + 1,
+    0,
+  );
+  firstDayOfThisMonth.setDate(1);
+  const [filter, setFilter] = useState<FromTo>({
+    from: firstDayOfThisMonth,
+    to: lastDayOfThisMonth,
   });
-
   return (
-    <div>
-      <form className="flex gap-2 rounded-md bg-c3 p-3">
-        <label htmlFor="start-date">From</label>
-        <input
-          id="start-date"
-          type="date"
-          className="px-1"
-          value={formatDate(filter.start)}
-          onChange={({ target: { value } }) =>
-            setFilter((p) => ({ ...p, start: new Date(value) }))
-          }
-        />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        changeDate(filter);
+      }}
+      className="flex items-center gap-2 rounded-md bg-c3 p-3"
+    >
+      <label htmlFor="start-date">Från</label>
+      <input
+        id="start-date"
+        type="date"
+        className="px-1"
+        value={dateToString(filter.from)}
+        onChange={({ target: { value } }) =>
+          setFilter((p) => ({ ...p, from: new Date(value) }))
+        }
+      />
 
-        <label htmlFor="end-date">To</label>
-        <input
-          id="end-date"
-          value={formatDate(filter.end)}
-          onChange={({ target: { value } }) =>
-            setFilter((p) => ({ ...p, end: new Date(value) }))
-          }
-        />
-      </form>
-      {children({ filter })}
-    </div>
+      <label htmlFor="end-date">Till</label>
+      <input
+        id="end-date"
+        type="date"
+        value={dateToString(filter.to)}
+        onChange={({ target: { value } }) =>
+          setFilter((p) => ({ ...p, to: new Date(value) }))
+        }
+      />
+      <Button callToAction type="submit">
+        Ok
+      </Button>
+    </form>
   );
 };
 
