2a91720eeb3d684c004cfa0137c046cf8408941a
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-30 06:41:06 +0100

    add admin link

--- src/app/_components/UserCard.tsx
+++ src/app/_components/UserCard.tsx
@@ -2,16 +2,18 @@ import { type Session } from "next-auth";
 import Image from "next/image";
 import { Logout } from "./Auth";
 import Icon from "~/icons/Icon";
+import Link from "next/link";
 
 type Props = { user: Session["user"] };
 
-const UserCard = ({ user: { name, image } }: Props) => {
+const UserCard = ({ user: { name, image, role } }: Props) => {
   return (
     <div className="flex items-center gap-2">
       <div className="group relative flex flex-col">
         <Icon icon="hamburgerMenu" className="h-10" />
         <div className="absolute right-0 top-full hidden flex-col gap-1 border border-black bg-c1 p-2 group-hover:flex">
           <p className="select-none text-nowrap">{name}</p>
+          {role === "admin" ? <Link href={"/admin"}>Admin</Link> : null}
           <Logout />
         </div>
       </div>
