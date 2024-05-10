e1abbf8877b566bcbbadab7c98a3917095d4032b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-10 08:00:05 +0200

    use new add temp button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 import Sample from "./_components/Sample";
-import AddTempButton from "./_components/AddTempButton";
 import FilterDates from "./_components/FilterDates";
 import parseSearch from "./utils/parseUrlDates";
 
 import dynamic from "next/dynamic";
+import AddTemp from "./_components/AddTemp";
+import { createSample } from "~/server/api/temps";
 const Chart = dynamic(() => import("./_components/charts/tempsByDay"), {
   ssr: false,
 });
@@ -27,10 +28,10 @@ const Home = async ({ searchParams }: Props) => {
   const data = await api.temp.read.query(dates);
   if (session) {
     return (
-      <main className="flex grow flex-col gap-4 bg-c1 p-5">
+      <main className="flex grow flex-col gap-4 bg-c1 md:p-5 py-2">
         <FilterDates />
         <Chart data={data} />
-        <AddTempButton />
+        <AddTemp submit={createSample} />
         <ul className="flex flex-col gap-2">
           {data.map((item) => (
             <Sample key={item.id} item={item} />
