6b6310d8b2bb629744c11e1ea8e54e3d92b128e9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-10 11:02:58 +0200

    Refactor

--- src/app/_components/Sample.tsx
+++ src/app/_components/Sample.tsx
@@ -1,37 +1,13 @@
-"use client";
-
-import { useRef, useState, type ReactNode } from "react";
-import Modal from "~/app/_components/Modal";
 import Icon from "~/icons/Icon";
-import { type RouterOutputs } from "~/trpc/shared";
-import RemoveModal from "./RemoveModal";
+import ConfirmRemoveSample from "./ConfirmRemoveSample";
+import type { TempId } from "~/zodSchemas";
 import SampleForm from "./SampleForm";
-import { api } from "~/trpc/react";
-import { useRouter } from "next/navigation";
+import { editSample } from "~/server/api/temps";
 
-type Temp = RouterOutputs["temp"]["read"][number];
 type Props = {
-  item: Temp;
+  item: TempId;
 };
 const Sample = ({ item }: Props) => {
-  const modal = useRef<HTMLDialogElement>(null);
-  const [modalContent, setModalContent] = useState<ReactNode>(null);
-  const toggleModal = () => {
-    if (!modal.current) {
-      return;
-    }
-    modal.current.hasAttribute("open")
-      ? modal.current.close()
-      : modal.current.showModal();
-  };
-  const router = useRouter();
-  const { mutate: update, isLoading: updating } = api.temp.update.useMutation({
-    onSuccess: () => {
-      router.refresh();
-      toggleModal();
-    },
-  });
-
   return (
     <li className="flex select-none items-center justify-between gap-2 rounded-md bg-c2 p-2">
       <p>
@@ -45,38 +21,18 @@ const Sample = ({ item }: Props) => {
         })}
       </p>
       <div className="flex items-center gap-2">
-        <Icon icon="temp" className="h-6 w-6 fill-c3" />
+        <Icon icon="temp" className="size-6 fill-c3" />
         <p>{item.temp.toFixed(2)}</p>
-        <button
-          onClick={() => {
-            setModalContent(
-              <SampleForm
-                disabled={updating}
-                onSubmit={({ date, temp }) =>
-                  update({ date, temp, id: item.id })
-                }
-                data={item}
-                onCancel={toggleModal}
-              />,
-            );
-            toggleModal();
+        <SampleForm
+          {...item}
+          submit={async ({ date, temp }) => {
+            "use server";
+            await editSample({ date, temp, id: item.id });
           }}
         >
-          <Icon icon="edit" className="h-6 w-6 fill-black" />
-        </button>
-        <button
-          onClick={() => {
-            setModalContent(
-              <RemoveModal toggleModal={toggleModal} id={item.id} />,
-            );
-            toggleModal();
-          }}
-        >
-          <Icon icon="close" className="h-6 w-6 fill-c4" />
-        </button>
-        <Modal toggleModal={toggleModal} ref={modal}>
-          {modalContent}
-        </Modal>
+          <Icon icon="edit" className="size-6 fill-black" />
+        </SampleForm>
+        <ConfirmRemoveSample id={item.id} />
       </div>
     </li>
   );
