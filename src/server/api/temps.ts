8b27d7e15562666c06adac139d2c6577984da815
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-10 11:03:18 +0200

    edit sample backend

--- src/server/api/temps.ts
+++ src/server/api/temps.ts
@@ -3,9 +3,10 @@
 import { createId } from "@paralleldrive/cuid2";
 import { db } from "../db";
 import { temps } from "../db/schema";
-import { type Temp } from "~/zodSchemas";
+import type { TempId, Temp } from "~/zodSchemas";
 import { getServerAuthSession } from "../auth";
 import { revalidatePath } from "next/cache";
+import { eq } from "drizzle-orm";
 
 export const createSample = async ({ temp, date }: Temp) => {
   const session = await getServerAuthSession();
@@ -19,5 +20,23 @@ export const createSample = async ({ temp, date }: Temp) => {
     createdAt: date,
     createdById: session.user.id,
   });
+  revalidatePath("/");
+};
+
+export const removeSample = async (id: string) => {
+  const session = await getServerAuthSession();
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  await db.delete(temps).where(eq(temps.id, id));
+  revalidatePath("/");
+};
+
+export const editSample = async ({ id, date, temp }: TempId) => {
+  const session = await getServerAuthSession();
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  await db.update(temps).set({ temp, updatedAt: date }).where(eq(temps.id, id));
   revalidatePath("/");
 };
